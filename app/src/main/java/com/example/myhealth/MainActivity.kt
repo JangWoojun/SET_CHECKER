b02aae1a861b3459ef02ebde995b3c174f1adb50
JangWoojun <woojun0107@naver.com>
2023-04-20 17:23:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myhealth/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myhealth
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.myhealth.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
