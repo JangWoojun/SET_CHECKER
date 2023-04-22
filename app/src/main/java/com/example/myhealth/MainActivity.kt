2ea3f398d5e23620b3e56c679b1f82566dac1114
JangWoojun <woojun0107@naver.com>
2023-04-22 20:28:35 +0900

    feat: 버튼 클릭 시 이벤트 효과 설정

--- app/src/main/java/com/example/myhealth/MainActivity.kt
+++ app/src/main/java/com/example/myhealth/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myhealth
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import com.example.myhealth.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
@@ -13,6 +15,35 @@ class MainActivity : Activity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var time = 120*1000
+
+        val restButton = findViewById<Button>(R.id.restButton)
+        val timeButton90 = findViewById<Button>(R.id.time90)
+        val timeButton120 = findViewById<Button>(R.id.time120)
+        val timeButton150 = findViewById<Button>(R.id.time150)
+        val timeButton180 = findViewById<Button>(R.id.time180)
+
+        restButton.setOnClickListener {
+            val intent = Intent(this, TimeActivity::class.java).putExtra("time", time)
+            startActivity(intent)
+        }
+
+        timeButton90.setOnClickListener {
+            time = 90*1000
+        }
+
+        timeButton120.setOnClickListener {
+            time = 120*1000
+        }
+
+        timeButton150.setOnClickListener {
+            time = 150*1000
+        }
+
+        timeButton180.setOnClickListener {
+            time = 180*1000
+        }
 
     }
 }
