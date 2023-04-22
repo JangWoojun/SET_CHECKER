b150c98dd62aabe2adec664c774ffe1a718a7968
JangWoojun <woojun0107@naver.com>
2023-04-22 23:45:45 +0900

    feat: 진동 이벤트 추가

--- app/src/main/java/com/example/myhealth/MainActivity.kt
+++ app/src/main/java/com/example/myhealth/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.myhealth
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.Button
+import android.widget.TextView
 import com.example.myhealth.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
@@ -16,33 +21,82 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var time = 120*1000
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        val duration = 500L
 
         val restButton = findViewById<Button>(R.id.restButton)
+        val timeButton60 = findViewById<Button>(R.id.time60)
         val timeButton90 = findViewById<Button>(R.id.time90)
-        val timeButton120 = findViewById<Button>(R.id.time120)
         val timeButton150 = findViewById<Button>(R.id.time150)
         val timeButton180 = findViewById<Button>(R.id.time180)
 
+        val setText = findViewById<TextView>(R.id.setText)
+
+        setText.text = Set.set.toString()
+
         restButton.setOnClickListener {
-            val intent = Intent(this, TimeActivity::class.java).putExtra("time", time)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                vibrator.vibrate(duration)
+            }
+            val intent = Intent(this, TimeActivity::class.java).putExtra("time", 120*1000)
             startActivity(intent)
+            finish()
+        }
+
+        timeButton60.setOnClickListener {
+            it.animate()
+                .scaleX(0.8f)
+                .scaleY(0.8f)
+                .setDuration(100)
+                .withEndAction {
+                    it.animate()
+                        .scaleX(1f)
+                        .scaleY(1f)
+                        .setDuration(100)
+                }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                vibrator.vibrate(duration)
+            }
+            val intent = Intent(this, TimeActivity::class.java).putExtra("time", 60*1000)
+            startActivity(intent)
+            finish()
         }
 
         timeButton90.setOnClickListener {
-            time = 90*1000
-        }
-
-        timeButton120.setOnClickListener {
-            time = 120*1000
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                vibrator.vibrate(duration)
+            }
+            val intent = Intent(this, TimeActivity::class.java).putExtra("time", 90*1000)
+            startActivity(intent)
+            finish()
         }
 
         timeButton150.setOnClickListener {
-            time = 150*1000
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                vibrator.vibrate(duration)
+            }
+            val intent = Intent(this, TimeActivity::class.java).putExtra("time", 150*1000)
+            startActivity(intent)
+            finish()
         }
 
         timeButton180.setOnClickListener {
-            time = 180*1000
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                vibrator.vibrate(duration)
+            }
+            val intent = Intent(this, TimeActivity::class.java).putExtra("time", 180*1000)
+            startActivity(intent)
+            finish()
         }
 
     }
