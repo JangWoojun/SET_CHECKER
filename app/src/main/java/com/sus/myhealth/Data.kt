9ea2a3db84e97b5268539a44dc136aa91daeb6ee
JangWoojun <woojun0107@naver.com>
2023-04-23 12:08:51 +0900

    feat: time 변수 추가

--- app/src/main/java/com/sus/myhealth/Data.kt
+++ app/src/main/java/com/sus/myhealth/Data.kt
@@ -1,9 +1,10 @@
-package com.example.myhealth
+package com.sus.myhealth
 
 import android.app.Application
 
-class Set : Application() {
+class Data : Application() {
     companion object {
         var set = 0
+        var time = 120000
     }
 }
